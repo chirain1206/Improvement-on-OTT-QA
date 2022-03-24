fdadf8819cbf798c5c7fc4a777183c61283f5de9
chirain1206 <chirain1206@gmail.com>
2022-03-25 04:32:28 +0800

    Update split_tables.py

--- split_tables.py
+++ split_tables.py
@@ -40,7 +40,29 @@ if __name__ == '__main__':
             with open(f'preprocessed_data/{args.split}_table_segments.json', 'w') as f:
                 json.dump(dict_results, f, indent=2)
             
-    elif args.split in ['dev', 'test']:
-        pass
+    elif args.split == 'all':
+        dict_results = {}
+        print("using {}".format(args.split))
+        if not os.path.exists(f'preprocessed_data/{args.split}_table_segments.json'):
+            with open(f'released_data/{args.split}.traced.json', 'r') as f:
+                data = json.load(f)
+
+            # extract tables corresponding to each training question
+            data = list(plain_tables.values())
+            results = []
+            with Pool(n_threads) as p:
+                results = list(
+                    tqdm(
+                        p.imap(split_table_segment, data, chunksize=16),
+                        total=len(data),
+                        desc="convert table to table segments",
+                    )
+                )
+
+            for table in tqdm(results):
+                dict_results[table[0]] = table[1:]  # first element in table indicate the uid of the table
+
+            with open(f'preprocessed_data/{args.split}_table_segments.json', 'w') as f:
+                json.dump(dict_results, f, indent=2)
     else:
         raise NotImplementedError
