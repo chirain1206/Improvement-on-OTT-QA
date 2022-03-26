2bf681453d9a451b10f88d5388a4580663f386cc
chirain1206 <chirain1206@gmail.com>
2022-03-26 16:37:49 +0800

    Update tmp_script.py

--- tmp_script.py
+++ tmp_script.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     with open('link_generator/all_passage_query.json', 'r') as f:
         querys = json.load(f)
 
-    absent_row = []
+    absent_rows = []
     table_names = list(data.keys())
     for cur_table_name in tqdm(table_names):
         for row_index, row in enumerate(data[cur_table_name]):
             if cur_table_name + f'_{row_index}' not in querys:
-                absent_row.append(cur_table_name + f'_{row_index}')
+                absent_rows.append(cur_table_name + f'_{row_index}')
 
-    with open('absent_row.json', 'w') as f:
-        json.dump(absent_row, f, indent=2)
+    with open('link_generator/absent_rows.json', 'w') as f:
+        json.dump(absent_rows, f, indent=2)
