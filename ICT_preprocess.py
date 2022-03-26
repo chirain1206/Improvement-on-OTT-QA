d1c0030eb5d68d2887ed0a19b0b06e5878560ace
chirain1206 <chirain1206@gmail.com>
2022-03-26 22:54:54 +0800

    ICT_preprocess

---
+++ ICT_preprocess.py
@@ -0,0 +1,39 @@
+from drqa import retriever
+from tqdm import tqdm
+from transformers import BertTokenizer
+from multiprocessing import Pool
+import argparse
+import logging
+import json
+import random
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+def generate_pseudo_train_sample(cur_fused_block):
+    
+
+    return pseudo_query, cur_fused_block[1]
+
+if __name__ == '__main__':
+    n_threads = 64
+
+    with open('preprocessed_data/train_fused_blocks.json', 'r') as f:
+        data = json.load(f)
+
+    block_names = list(data.keys())
+    random.shuffle(block_names)
+    shuffle_data = [[name, data[name]] for name in block_names]
+
+    with Pool(n_threads) as p:
+        results = list(
+            tqdm(
+                p.imap_unordered(generate_pseudo_train_sample, shuffle_data),
+                total=len(block_names),
+                desc='Generate training samples for ICT',
+            )
+        )
---
+++ convert_query_to_url.py
@@ -0,0 +1,37 @@
+from drqa import retriever
+from tqdm import tqdm
+from transformers import BertTokenizer
+from multiprocessing import Pool
+import argparse
+import logging
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, required=True)
+parser.add_argument('--option', type=str, default='bm25')
+parser.add_argument('--split', type=str, default='train')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    logger.info('Initializing ranker...')
+    ranker = retriever.get_class(args.option)(bm25_path=args.model, strict=False)
+    tokenizer = BertTokenizer.from_pretrained('bert-large-uncased', do_lower_case=True, cache_dir='/tmp/')
+    all_query_url = {}
+    with open('link_generator/all_passage_query.json', 'r') as f:
+        querys = json.load(f)
+
+    for segment_name, query_lst in list(tqdm(querys.items())):
+        all_query_url[segment_name] = []
+        for query in query_lst:
+            if len(ranker.closest_docs(generated_query, 1)[0]) > 0:
+                all_query_url[segment_name].append(ranker.closest_docs(generated_query, 1)[0][0])
+
+    with open('link_generator/all_url.json', 'w') as f:
+        json.dump(all_query_url, f, indent=2)
