c7d187f027ebacfb0254874930e074617ba55b88
chirain1206 <chirain1206@gmail.com>
2022-03-26 16:25:55 +0800

    Create tmp_script.py

---
+++ tmp_script.py
@@ -0,0 +1,17 @@
+import json
+
+if __name__ == '__main__':
+    with open('preprocessed_data/train_table_segments.json', r) as f:
+        data = json.load(f)
+
+    with open('link_generator/all_passage_query.json', 'r') as f:
+        querys = json.load(f)
+
+    absent_row = []
+    table_names = list(data.keys())
+    for name in table_names:
+        if name not in querys:
+            absent_row.append(name)
+
+    with open('absent_row.json', 'w') as f:
+        json.dump(absent_row, f, indent=2)
