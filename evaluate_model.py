bb1a643ab3b29ca5cf29f13b2607982edc64db85
chirain1206 <chirain1206@gmail.com>
2022-04-01 20:48:39 +0800

    evaluate retriever

---
+++ evaluate_model.py
@@ -0,0 +1,117 @@
+import argparse
+import logging
+import json
+import os
+import sys
+import random
+from train_retriever import VectorizeModel
+from transformers import (BertConfig, BertTokenizer, BertModel)
+from torch import nn
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--eval_size', type=int, default=1000)
+parser.add_argument(
+    "--load_model_path",
+    default="",
+    type=str,
+    required=True,
+    help="Path to the model that have been trained"
+)
+parser.add_argument(
+    "--model_name_or_path",
+    default="bert-base-uncased",
+    type=str,
+    help="Path to pre-trained model or shortcut name selected in the list"
+)
+parser.add_argument(
+    "--cache_dir",
+    default="/tmp/",
+    type=str,
+    help="Where do you want to store the pre-trained models downloaded from s3",
+)
+parser.add_argument(
+    "--eval_option",
+    default="",
+    type=str,
+    help="retriever, reader, or both",
+)
+parser.add_argument(
+    "--top_k",
+    default=15,
+    type=int,
+    help="k value for top_k retrieval",
+)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    with open('released_data/dev.traced.json', 'r') as f:
+        data = json.load(f)
+    data = random.shuffle(data)
+    data = data[:args.eval_size]
+
+    if args.eval_option == 'retriever':
+        # load the matrix of candidates
+        candidate_info = torch.load('./preprocessed_data/dev_candidates.pth')
+        IDX2BLOCK = candidate_info['IDX2BLOCK']
+        BLOCK2IDX = candidate_info['BLOCK2IDX']
+        candidate_matrix = candidate_info['candidate_matrix']
+
+        query_config = BertConfig.from_pretrained(
+            args.model_name_or_path,
+            cache_dir=args.cache_dir
+        )
+        query_tokenizer = BertTokenizer.from_pretrained(
+            args.model_name_or_path,
+            do_lower_case=args.do_lower_case,
+            cache_dir=args.cache_dir
+        )
+        args.orig_dim = query_config.hidden_size
+        args.proj_dim = 128
+
+        query_model = VectorizeModel(BertModel, args.model_name_or_path, query_config, len(query_tokenizer),
+                                     args.cache_dir,
+                                     args.orig_dim, args.proj_dim)
+        query_model_path = os.path.join(args.load_model_path, 'pytorch_model.bin')
+        query_model.load_state_dict(torch.load(query_model_path))
+
+        num_succ = 0
+        num_fin_questions = 0
+        for trace_question in data:
+            answer_row = set()
+            for node in answer_node:
+                answer_row.add(node[1][0])
+            answer_segment = set()
+            for row in answer_row:
+                answer_segment.add(trace_question['table_id'] + f'_{row}')
+
+            # compute vector for the question
+            query = trace_question['question']
+            query_tokens = '[CLS] ' + question + ' [SEP]'
+            query_tokens = query_tokenizer.tokenize(query_tokens)
+            query_input_tokens = torch.LongTensor([query_tokenizer.convert_tokens_to_ids(query_tokens)])
+            query_types = torch.LongTensor([[0] * len(query_tokens)])
+            query_masks = torch.LongTensor([[1] * len(query_tokens)])
+            query_cls = query_model(query_input_tokens, query_input_types, query_input_masks)
+            print(query_cls.size())
+
+            # compute similarity score
+            retrieval_score = nn.functional.cosine_similarity(query_cls, candidate_matrix, dim=1)
+            _, indices = torch.topk(retrieval_score, args.top_k)
+
+            for i in range(args.top_k):
+                if IDX2BLOCK[indices[i].item()] in answer_segment:
+                    num_succ += 1
+                    break
+            num_fin_questions += 1
+            sys.stdout.write('finished {}/{}; HITS@{} = {} \r'.format(num_fin_questions, len(data), args.top_k,
+                                                                      num_succ / num_fin_questions))
+
+        print('finished {}/{}; HITS@{} = {} \r'.format(num_fin_questions, len(data), args.top_k,
+                                                       num_succ / num_fin_questions))
---
+++ retriever/encode_candidates.py
@@ -0,0 +1,93 @@
+import argparse
+import logging
+import json
+import os
+import sys
+from train_retriever import VectorizeModel
+from transformers import (BertConfig, BertTokenizer, BertModel)
+import torch
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(current_path))
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--load_model_path",
+    default="",
+    type=str,
+    required=True,
+    help="Path to the model that have been trained"
+)
+parser.add_argument(
+    "--model_name_or_path",
+    default="bert-base-uncased",
+    type=str,
+    help="Path to pre-trained model or shortcut name selected in the list"
+)
+parser.add_argument(
+    "--cache_dir",
+    default="/tmp/",
+    type=str,
+    help="Where do you want to store the pre-trained models downloaded from s3",
+)
+parser.add_argument(
+    "--candidates_file",
+    default=None,
+    type=str,
+    help="Path to the file includes candidate fused blocks",
+)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    block_config = BertConfig.from_pretrained(
+        args.model_name_or_path,
+        cache_dir=args.cache_dir
+    )
+    block_tokenizer = BertTokenizer.from_pretrained(
+        args.model_name_or_path,
+        do_lower_case=args.do_lower_case,
+        cache_dir=args.cache_dir
+    )
+    block_tokenizer.add_tokens(["[TAB]", "[TITLE]", "[ROW]", "[MAX]", "[MIN]", "[EAR]", "[LAT]"])
+    args.orig_dim = block_config.hidden_size
+    args.proj_dim = 128
+    block_model = VectorizeModel(BertModel, args.model_name_or_path, block_config, len(block_tokenizer), args.cache_dir,
+                                 args.orig_dim, args.proj_dim, for_block=True)
+    block_model_path = os.path.join(args.load_model_path, 'pytorch_model.bin')
+    block_model.load_state_dict(torch.load(block_model_path))
+    candidate_matrix = None
+
+    with oepn(args.candidates_file, 'r') as f:
+        candidates = json.load(f)
+
+    IDX2BLOCK = list(candidates.keys())
+    BLOCK2IDX = {block_name: i for i, block_name in enumerate(IDX2BLOCK)}
+
+    for candidate_name in IDX2BLOCK:
+        tokens, token_type, token_mask = candidates[candidate_name]
+
+        # add [CLS] token to front of the fused block
+        tokens = ["[CLS]"] + tokens
+        tokens = torch.LongTensor([block_tokenizer.convert_tokens_to_ids(tokens)])
+        type = torch.LongTensor([[0] + token_type])
+        mask = torch.LongTensor([[1] + token_mask])
+
+        # torch.Size([1, 128])
+        candidate_vec = block_model(tokens, type, mask)
+
+        if candidate_matrix == None:
+            candidate_matrix = candidate_vec
+        else:
+            candidate_matrix = torch.cat(candidate_matrix, candidate_vec)
+
+    assert candidate_matrix.size()[0] == len(IDX2BLOCK)
+
+    save_data = {"IDX2BLOCK":IDX2BLOCK, "BLOCK2IDX":BLOCK2IDX, "candidate_matrix":candidate_matrix}
+    torch.save(save_data, '../preprocessed_data/dev_candidates.pth')
