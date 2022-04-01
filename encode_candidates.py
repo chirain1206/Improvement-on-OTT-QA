2d47b22370903269e30b9b82d4a624930d70dbe4
chirain1206 <chirain1206@gmail.com>
2022-04-01 22:28:22 +0800

    Update encode_candidates.py

--- encode_candidates.py
+++ encode_candidates.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
         tokens, token_type, token_mask = tuple(t.to(args.device) for t in batch)
 
         # torch.Size([1, 128])
-        candidate_vec = block_model(tokens, type, mask)
+        candidate_vec = block_model(tokens, token_type, token_mask)
 
         if candidate_matrix == None:
             candidate_matrix = candidate_vec
