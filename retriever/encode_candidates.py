ddaa10461976f42f535c1aeaa9ff5c13c21f2bf7
chirain1206 <chirain1206@gmail.com>
2022-04-01 21:07:56 +0800

    Update encode_candidates.py

--- retriever/encode_candidates.py
+++ retriever/encode_candidates.py
@@ -7,8 +7,7 @@ from train_retriever import VectorizeModel
 from transformers import (BertConfig, BertTokenizer, BertModel)
 import torch
 
-current_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(current_path))
+sys.path.append(os.path.dirname(".."))
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
