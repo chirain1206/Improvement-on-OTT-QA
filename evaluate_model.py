5a7c43c6eacff9aefc7d2b85f5420f90ca5dd653
chirain1206 <chirain1206@gmail.com>
2022-04-02 13:58:30 +0800

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
                 }
 
                 reader_outputs = reader_model(**reader_inputs)
-                print(reader_outputs)
+                print(reader_outputs[0].size())
 
                 break
             break
