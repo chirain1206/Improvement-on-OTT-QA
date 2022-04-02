302c526b65292a1dc8e4330ecf87d2d3d8de25e9
chirain1206 <chirain1206@gmail.com>
2022-04-03 02:06:57 +0800

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -234,8 +234,10 @@ if __name__ == '__main__':
         num_fin_questions = 0
         for trace_question in data:
             if args.predict_title:
-                input_title = 'In ' ' [SEP] ' + ' [SEP] ' + ' [ENT] ' + trace_question['question'] + ' [ENT] '
+                input_title = ' [ENT] ' + trace_question['question'] + ' [ENT] '
                 prediction = sample_sequence(gpt_model, gpt_tokenizer, 16, input_title, [], temperature=0)
+                if len(prediction) != 0:
+                    print(prediction)
                 query = trace_question['question'] + prediction
             else:
                 query = trace_question['question']
